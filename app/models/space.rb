16d8294ac596aa4c4efbba5b72f6c93a4cf9c936
Boyan Levchev <boyanlevchev@gmail.com>
2020-02-28 12:36:44 +0000

    edited the card to look sleeker

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,6 +1,6 @@
 class Space < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: 'user_id'
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
 
   has_many_attached :photos
